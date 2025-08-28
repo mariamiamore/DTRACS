a39a7e4b804914c04dc671af5c640f0e302be249
mariamiamore <165545199+mariamiamore@users.noreply.github.com>
2025-08-28 22:31:28 +0800

    added new card, responsive, still need changes

--- src/components/CardLogo/CardLogo.jsx
+++ src/components/CardLogo/CardLogo.jsx
@@ -6,22 +6,42 @@ import WhiteLogo from "../../assets/images/sgod-white.png";
 
 const CardLogo = () => {
   const [hovered, setHovered] = useState(false);
+  const [hoveredCard, setHoveredCard] = useState(false);
   const navigate = useNavigate();
 
   return (
-    <div
-      className="card"
-      style={{ backgroundImage: `url(${hovered ? WhiteLogo : BlueLogo})` }}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-      onClick={() => navigate("/SGOD")}   // 👈 navigate when clicked
+  <div className="card-container">
+    {/* SGOD Card */}
+      <div
+        className="card"
+        style={{ backgroundImage: `url(${hovered ? WhiteLogo : BlueLogo})` }}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        onClick={() => navigate("/SGOD")}   // 👈 navigate when clicked
     >
-      <div className={`card-text ${hovered ? "hovered" : ""}`}>
-        <h1 className="title">
-          School Governance and <br /> Operations Division
-        </h1>
+        <div className={`card-text ${hovered ? "hovered" : ""}`}>
+          <h1 className="title">
+            School Governance and <br /> Operations Division
+          </h1>
+        </div>
       </div>
-    </div>
+
+    {/* CID Card */}
+      <div
+        className="card"
+        style={{ backgroundImage: `url(${hoveredCard ? WhiteLogo : BlueLogo})` }}
+        onMouseEnter={() => setHoveredCard(true)}
+        onMouseLeave={() => setHoveredCard(false)}
+        onClick={() => navigate("/SGOD")}   // navigate to CID page on click
+      >
+        <div className={`card-text ${hoveredCard ? "hovered" : ""}`}>
+          <h1 className="title">
+            Curriculum Implementation <br /> Division
+          </h1>
+        </div>
+      </div>
+
+  </div>
   );
 };
 
